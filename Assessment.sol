9706bdef350ce44108a5dff078e5985a90e820fc
Himanshu singh <165463135+flexhiman@users.noreply.github.com>
2024-09-11 15:25:12 +0530

    Create Assessment.sol

---
+++ Assessment.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance; // Corrected spelling
+    uint256 public transactionCount; // Corrected spelling
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+        transactionCount = 0; // Initialize transaction count
+    }
+
+    function getBalance() public view returns(uint256) {
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        balance += _amount;
+        transactionCount++; // Increment transaction count
+
+        assert(balance == _previousBalance + _amount);
+
+        emit Deposit(_amount);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        balance -= _withdrawAmount;
+        transactionCount++; // Increment transaction count
+
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        emit Withdraw(_withdrawAmount);
+    }
+
+    function numberOfTransactions() public view returns (uint256) {
+        return transactionCount;
+    }
+}
